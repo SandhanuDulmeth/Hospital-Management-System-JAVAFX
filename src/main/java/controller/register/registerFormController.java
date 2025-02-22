a285bae95d415465ed7ba105e6ca8d4b465d2123
SandhanuDulmeth <sandhanu12@gmail.com>
2025-02-22 18:41:27 +0530

    Commit

--- src/main/java/controller/register/registerFormController.java
+++ src/main/java/controller/register/registerFormController.java
@@ -1,14 +1,17 @@
 package controller.register;
 
-
 import util.ServiceType;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import model.Users;
 import org.jasypt.util.text.BasicTextEncryptor;
@@ -33,56 +36,58 @@ public class registerFormController implements Initializable {
     public JFXTextField txtEmail;
     public JFXTextField txtPassword;
     public JFXButton btnAdd;
-
-    private final RegisterService registerService= ServiceFactory.getInstance().getServiceType(ServiceType.REGISTER);
     public JFXButton GetOTPID;
+    public JFXTextField TextOTPId;
+    public String OTPFinal = "";
+
+    private final RegisterService registerService = ServiceFactory.getInstance().getServiceType(ServiceType.REGISTER);
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        btnAdd.setDisable(true);
+        int i = registerService.geLastUserId();
+        txtId.setText("New ID : " + i);
+    }
 
     public void btnAddOnAction(ActionEvent actionEvent) {
         if (isFieldEmpty(txtName, "Name") || isFieldEmpty(txtEmail, "Email") || isFieldEmpty(txtPassword, "Password")) {
             return;
         }
+        if (!TextOTPId.getText().equals(OTPFinal)) {
+            new Alert(Alert.AlertType.ERROR, "OTP is Wrong").show();
+            return;
+        }
 
-        String key="12345";
-        BasicTextEncryptor basicTextEncryptor=new BasicTextEncryptor();
+        String key = "12345";
+        BasicTextEncryptor basicTextEncryptor = new BasicTextEncryptor();
         basicTextEncryptor.setPassword(key);
         String encrypt = basicTextEncryptor.encrypt(txtPassword.getText());
 
         Boolean b = registerService.addUser(new Users(txtName.getText(), txtEmail.getText(), encrypt));
-        if (b){
-            new Alert(Alert.AlertType.INFORMATION,"Added");
-        }else{
-            new Alert(Alert.AlertType.ERROR,"Now Added");
+        if (b) {
+            new Alert(Alert.AlertType.INFORMATION, "Added").show();
+        } else {
+            new Alert(Alert.AlertType.ERROR, "Not Added").show();
             return;
         }
-        Stage stageClose =(Stage) btnAdd.getScene().getWindow() ;
-
+        Stage stageClose = (Stage) btnAdd.getScene().getWindow();
         try {
             stageClose.setScene(new Scene(FXMLLoader.load(getClass().getResource("../../view/Login_form.fxml"))));
             stageClose.show();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        int i = registerService.geLastUserId();
-        txtId.setText("New ID : "+i);
     }
 
     public void btnBackOnAction(ActionEvent actionEvent) {
-        Stage stageClose =(Stage) btnAdd.getScene().getWindow() ;
+        Stage stageClose = (Stage) btnAdd.getScene().getWindow();
         try {
             stageClose.setScene(new Scene(FXMLLoader.load(getClass().getResource("../../view/Login_form.fxml"))));
             stageClose.show();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
-
-
-//
 
     private boolean isFieldEmpty(JFXTextField field, String fieldName) {
         if (field == null || field.getText().trim().isEmpty()) {
@@ -100,54 +105,91 @@ public class registerFormController implements Initializable {
         alert.showAndWait();
     }
 
-    public void btnGetOTPOnAction(ActionEvent actionEvent) throws MessagingException {
-        String recipientEmail=txtEmail.getText();
-        sendEmail(recipientEmail);
+    // When the Get OTP button is clicked, the email is sent with a loading animation.
+    public void btnGetOTPOnAction(ActionEvent actionEvent) {
+        String recipientEmail = txtEmail.getText();
+        sendEmailWithLoading(recipientEmail);
     }
 
-    private void sendEmail(String recipientEmail) throws MessagingException {
-        Properties properties = new Properties();
+    private void sendEmailWithLoading(String recipientEmail) {
+        // Create and display a loading stage with a ProgressIndicator.
+        Stage loadingStage = new Stage();
+        ProgressIndicator progressIndicator = new ProgressIndicator();
+        StackPane loadingRoot = new StackPane(progressIndicator);
+        Scene loadingScene = new Scene(loadingRoot, 100, 100);
+        loadingStage.setScene(loadingScene);
+        loadingStage.setTitle("Sending Email...");
+        loadingStage.show();
 
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.port", "587");
-
-        String myEmail = "bruno12mendis740cj30x@gmail.com";
-        String password = "yrkd xity mqmv hktk";
-
-
-        Session session=Session.getInstance(properties, new Authenticator() {
+        // Run the email sending logic in a background thread.
+        Task<Void> task = new Task<Void>() {
             @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(myEmail,password);
+            protected Void call() throws Exception {
+                try {
+                    Properties properties = new Properties();
+                    properties.put("mail.smtp.auth", "true");
+                    properties.put("mail.smtp.starttls.enable", "true");
+                    properties.put("mail.smtp.host", "smtp.gmail.com");
+                    properties.put("mail.smtp.port", "587");
+
+                    String myEmail = "bruno12mendis740cj30x@gmail.com";
+                    String password = "yrkd xity mqmv hktk";
+
+                    Session session = Session.getInstance(properties, new Authenticator() {
+                        @Override
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(myEmail, password);
+                        }
+                    });
+
+                    OTPFinal = generateOTP();
+                    Message message = prepareMessage(session, myEmail, recipientEmail, OTPFinal);
+                    if (message != null) {
+                        Transport.send(message);
+                        Platform.runLater(() -> {
+                            new Alert(Alert.AlertType.INFORMATION, "Send Email Successfully").show();
+                            btnAdd.setDisable(false);
+                        });
+                    } else {
+                        Platform.runLater(() -> {
+                            new Alert(Alert.AlertType.ERROR, "Please Try Again").show();
+                        });
+                        OTPFinal = "";
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Platform.runLater(() -> {
+                        new Alert(Alert.AlertType.ERROR, "Email Sending Failed!").show();
+                    });
+                }
+                return null;
             }
-        });
-        Message message=prepareMessage(session,myEmail,recipientEmail,generateOTP());
-        if (message!=null){
-            new Alert(Alert.AlertType.INFORMATION,"Send Email Successfully").show();
-        }else{
-            new Alert(Alert.AlertType.ERROR,"Please Try Again").show();
-        }
-        Transport.send(message);
+        };
+
+        // Close the loading stage after the task is finished.
+        task.setOnSucceeded(e -> loadingStage.close());
+        task.setOnFailed(e -> loadingStage.close());
+
+        Thread thread = new Thread(task);
+        thread.setDaemon(true);
+        thread.start();
     }
 
     private Message prepareMessage(Session session, String myEmail, String recipientEmail, String OTP) {
-        try{
-    Message message=new MimeMessage(session);
-    message.setFrom(new InternetAddress(myEmail));
-    message.setRecipients(Message.RecipientType.TO,new InternetAddress[]{
-            new InternetAddress(recipientEmail)
-    });
-    message.setSubject("Messages");
-    message.setText(OTP);
-    return message;
-        }catch (Exception e){
-            Logger.getLogger(registerFormController.class.getName()).log(Level.SEVERE,null,e);
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(myEmail));
+            message.setRecipients(Message.RecipientType.TO, new InternetAddress[]{
+                    new InternetAddress(recipientEmail)
+            });
+            message.setSubject("Messages");
+            message.setText(OTP);
+            return message;
+        } catch (Exception e) {
+            Logger.getLogger(registerFormController.class.getName()).log(Level.SEVERE, null, e);
         }
         return null;
     }
-
 
     private String generateOTP() {
         String otp = "";
@@ -157,7 +199,4 @@ public class registerFormController implements Initializable {
         }
         return otp;
     }
-
-
-
 }
